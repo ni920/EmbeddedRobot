cccd8a02a38506cae6c9bfe65dc8097d9bdea341
Nico <40561468+ni920@users.noreply.github.com>
2023-03-23 22:47:37 +0100

    Update robot.c

--- src/robot.c
+++ src/robot.c
@@ -3,24 +3,75 @@
  \author	${user}
  \date		${date}
  \brief		Simple Hello World! for the Ev3
-*/
+ */
 
 #include <ev3.h>
 
-int main(void)
-{
 
-	// INFO This code template works only with recent versions of the API. If TermPrintln is not found,
-	//      please update the API or use the "Hello World EV3 Project Old API" as template.
+const int DISTANCE_TO_STOP = 100;
+const int DRIVE_SPEED = 10;
+const int DRIVE_STOP = 0;
+const int KICK_ROTATION_SPEED = 50;
+const int WHITE = 6;
+const int RED = 5;
+const int KICK_SPEED = 100;
+const int ANGLE_360 = 360;
 
+int driveForward();
+void exitRobo();
+void init();
+void kick(int);
+void kickWithMotor();
 
-	//TODO Place here your variables
+int main(void) {
+	// all initial values
+	init();
 
+	while (true) {
+		exitRobo();
+		if (readSensor(IN_4) == WHITE || readSensor(IN_4) == RED ) {
+			Off(OUT_AD);
+			kickWithMotor();
+		}else {
+			driveForward();
+		}
 
-	//TODO Place here your code
-	TermPrintln("Hello World!");
-	TermPrintf("Press ENTER to exit");
-	ButtonWaitForPress(BUTTON_ID_ENTER);
+	}
 
 	return 0;
 }
+
+void init() {
+	setAllSensorMode(US_DIST_MM, NO_SEN, NO_SEN, COL_COLOR);
+}
+
+int driveForward() {
+	int distance = readSensor(IN_1);
+	TermPrintf("Distance: %d \n", distance);
+	if (distance > DISTANCE_TO_STOP) {
+		OnFwdSync(OUT_AD, DRIVE_SPEED);
+		return 1;
+	} else {
+		Off(OUT_AD);
+		return 0;
+	}
+}
+
+void exitRobo() {
+	if (ButtonIsDown(BUTTON_ID_ENTER)) {
+		FreeEV3();
+	}
+}
+
+void kick(int state) {
+	if (state == 0) {
+		RotateMotor(OUT_A, KICK_ROTATION_SPEED, 360);
+		RotateMotor(OUT_A, KICK_ROTATION_SPEED, -360);
+	}
+}
+
+void kickWithMotor() {
+	RotateMotor(OUT_C, KICK_SPEED, ANGLE_360);
+}
+
+
